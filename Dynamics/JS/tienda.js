89d2f7611ca2f1ee3b01f8dea8045d8b4afca275
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-16 11:18:39 -0600

    Fin de maquetado

---
+++ Dynamics/JS/tienda.js
@@ -0,0 +1,22 @@
+/*===========================Objetos importantes====================*/
+
+class Producto
+{
+    constructor(_nombre, _imagen, _descrip, _precio)
+    {
+        this.nombre = _nombre;
+        this.descripcion = _descrip;
+        this.precio = _precio; 
+        
+    }
+}
+class Products_queue
+{
+    constructor(_producto)
+    {
+        
+    }
+}
+/*===========================Elementos del documento====================*/
+
+let area_productos_obj = document.getElementById("products-area");
