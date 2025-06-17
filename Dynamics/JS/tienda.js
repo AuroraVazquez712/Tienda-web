cdf83629f0fd507f8582d32ae32977d01ff5957a
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-16 18:17:22 -0600

    feat: Elimino JS sobrante

--- Dynamics/JS/tienda.js
+++ Dynamics/JS/tienda.js
@@ -36,11 +36,10 @@ Products_Stack =
     new Producto("Manzana", "../../Statics/Img/manzana.png", "Deliciosa comida", "25$ mxn"),
     
     new Producto("Marca Textos", "../../Statics/Img/marcatextos.webp", "Marcatextos naranja", "15$ mxn"),
-    new Producto("Mascota", "../../Statics/Img/mascota.webp", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
+    new Producto("Mascota", "../../Statics/Img/mascota.webp", "Peluche Gato 30x20cm", "250$ mxn"),
+    new Producto("Monedero", "../../Statics/Img/monedero.webp", "Deliciosa comida", "50$ mxn"),
     
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    
+    new Producto("Moto deportiva", "../../Statics/Img/moto.jpg", "Deliciosa comida", "15,000$ mxn"),
+    new Producto("Rollo papel baño", "../../Statics/Img/servilletaEnCilindro.webp", "Deliciosa comida", "15$ mxn"),
+    new Producto("Tubo papel baño", "../../Statics/Img/servilletaTransparenteEnCilindro.jpg", "Deliciosa comida", "10$ mxn"),
 ]
