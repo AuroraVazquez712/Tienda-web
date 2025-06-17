d424742da5d055b3a16011a33d483bf034863e17
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-16 20:05:24 -0600

    fix: Nuevo JS

--- Dynamics/JS/tienda.js
+++ Dynamics/JS/tienda.js
@@ -1,9 +1,7 @@
 /*===========================Elementos del documento====================*/
-let area_productos_obj = document.getElementById("products-area");
-
+const payArea = document.getElementById("products-area");
 
 /*===========================Objetos importantes====================*/
-
 class Producto
 {
     constructor(_nombre, _imagen, _descrip, _precio)
@@ -11,35 +9,36 @@ class Producto
         this.nombre = _nombre;
         this.imagen = _imagen;
         this.descripcion = _descrip;
-        this.precio = _precio;   
+        this.precio = _precio; 
     }
 }
-class Products_queue
-{
-    constructor(_productos)
-    {
-        this.products_queue = Array();
-        this.products_queue.push(_productos);
-
-        let new_div = document.createElement("div");
-        new_div.className("product");
-    }
-}
-
-let Products_queue_obj = new Products_queue();
 
 Products_Stack = 
 [
     new Producto("Charola","../../Statics/Img/charola.webp","Charola de plastico 30x15cm","100$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Manzana", "../../Statics/Img/manzana.png", "Deliciosa comida", "50$ mxn"),
+    new Producto("Comida", "../../Statics/Img/comida.jpg", "Deliciosa comida", "50$ mxn"),
+    new Producto("Manzana", "../../Statics/Img/manzana.jpg", "Deliciosa comida", "25$ mxn"),
     
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
+    new Producto("Marca Textos", "../../Statics/Img/marcatextos.webp", "Marcatextos naranja", "15$ mxn"),
+    new Producto("Mascota", "../../Statics/Img/mascota.webp", "Peluche Gato 30x20cm", "250$ mxn"),
+    new Producto("Monedero", "../../Statics/Img/monedero.webp", "Monedero perfecto 15x20cm", "50$ mxn"),
     
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
-    
+    new Producto("Moto deportiva", "../../Statics/Img/moto.jpg", "Moto 20v 200,000 HP", "15,000$ mxn"),
+    new Producto("Rollo papel baño", "../../Statics/Img/servilletaEnCilindro.webp", "Doble hoja 10mil hojas", "15$ mxn"),
+    new Producto("Tubo papel baño", "../../Statics/Img/servilletaTransparenteEnCilindro.jpg", "1M hojas invisibles", "10$ mxn"),
 ]
+for(iterador in Products_Stack)
+{
+    let new_product = document.createElement("div");
+    new_product.className = "Producto";   
+    
+    new_product.innerHTML = "<div class='Producto'>" +
+    "<h4 class = 'product-title'>" + Products_Stack[iterador].nombre + "</h4>" +
+    "<p class = 'product-bio'>" + Products_Stack[iterador].descripcion + "</p>" +
+    "<p class = 'product-value'>" + Products_Stack[iterador].precio + "</p>" +
+    "<button class = 'product-bt'>Comprar</button>" +
+    "<img class = 'product-img' src =' >" + Products_Stack[iterador].imagen + "'></div>";
+
+
+    payArea.appendChild(new_product);
+}
