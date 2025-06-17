b1d301dbc6f5645fa9c6d1a11750103dcac86416
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-16 18:21:27 -0600

    feat: For stack

--- Dynamics/JS/tienda.js
+++ Dynamics/JS/tienda.js
@@ -1,9 +1,7 @@
 /*===========================Elementos del documento====================*/
-let area_productos_obj = document.getElementById("products-area");
-
+const area_productos_obj = document.getElementById("products-area");
 
 /*===========================Objetos importantes====================*/
-
 class Producto
 {
     constructor(_nombre, _imagen, _descrip, _precio)
@@ -14,22 +12,9 @@ class Producto
         this.precio = _precio;   
     }
 }
-class Products_queue
-{
-    constructor(_productos)
-    {
-        this.products_queue = Array();
-        this.products_queue.push(_productos);
 
-        let new_div = document.createElement("div");
-        new_div.className("product");
-        
-    }
-}
-
-let Products_queue_obj = new Products_queue();
-
-Products_Stack = 
+/*===========================Lista de productos====================*/
+stack = 
 [
     new Producto("Charola","../../Statics/Img/charola.webp","Charola de plastico 30x15cm","100$ mxn"),
     new Producto("Comida", "../../Statics/Img/comida.png", "Deliciosa comida", "50$ mxn"),
@@ -43,3 +28,10 @@ Products_Stack =
     new Producto("Rollo papel baño", "../../Statics/Img/servilletaEnCilindro.webp", "Deliciosa comida", "15$ mxn"),
     new Producto("Tubo papel baño", "../../Statics/Img/servilletaTransparenteEnCilindro.jpg", "Deliciosa comida", "10$ mxn"),
 ]
+
+let Products_queue = Array();
+
+for(iterador in stack)
+{
+    
+}
